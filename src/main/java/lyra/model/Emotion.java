4848117abf4e05919c8531666be694633c603c3b
Oceana Gershberg <75436638+oceana1129@users.noreply.github.com>
2025-11-10 14:17:48 -0800

    Added information for Emotion.java and EmotionDao.java with CRUD operations.

--- src/main/java/lyra/model/Emotion.java
+++ src/main/java/lyra/model/Emotion.java
@@ -5,11 +5,30 @@ package lyra.model;
  */
 public class Emotion {
     /** Attributes */
-
+    private int emotionId;
+    private String name;
 
     /** Constructor */
-
+    public Emotion(int emotionId, String name) {
+        this.emotionId = emotionId;
+        this.name = name;
+    }
 
     /** Getters and setters */
+    public int getEmotionId() {
+        return emotionId;
+    }
+
+    public void setEmotionId(int emotionId) {
+        this.emotionId = emotionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
